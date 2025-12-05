c04ab6c4f3dcd4184bb5e45556fb5d7a5c90c629
swavish164 <89749822+swavish164@users.noreply.github.com>
2025-12-05 23:07:13 +0000

    Adding graphs

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+import seaborn as sns
 from sklearn.cluster import HDBSCAN
 import matplotlib.pyplot as plt
-import hdbscan
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+import matplotlib
+# matplotlib.use("Agg")
+plt.ioff()
 scaler = StandardScaler()
-
 
 lapMatrix = pd.read_pickle("2025Silverstone.pkl")
 lapMatrix = lapMatrix.dropna(subset=[('VER', 'Lap Data')])
 # print(lapMatrix.columns)
 drivers = lapMatrix.columns.get_level_values(0).unique()
 allLaps = []
+allDrivers = []
+palette = sns.color_palette("hls", len(drivers))
+driverColours = {driver: palette[i] for i, driver in enumerate(drivers)}
 
 columns = [
     "upshifts",
@@ -26,6 +31,9 @@ columns = [
     "avCornerSpeedDiff",
     "avApexThrottle",]
 
+data = {col: [] for col in columns}
+
+
 for driver in drivers:
     driverData = lapMatrix[(driver, 'Lap Data')]
     for i in range(lapMatrix.shape[0]):
@@ -33,11 +41,30 @@ for driver in drivers:
         if isinstance(currentDriverLap, dict):
             lapData = list(currentDriverLap.values())
             allLaps.append(lapData)
+            allDrivers.append(driver)
+            for i, col in enumerate(columns):
+                value = lapData[i]
+                data[col].append({"driver": driver, "value": value})
+
+df = pd.DataFrame({
+    "driver": allDrivers,
+    **{col: [row["value"] for row in data[col]] for col in columns}
+})
+
+sns.pairplot(
+    df,
+    vars=columns,
+    hue="driver",
+    palette=driverColours,
+    plot_kws={"s": 10, "alpha": 0.6}
+)
+plt.show()
 
 allLapsDF = pd.DataFrame(allLaps, columns=columns)
 scaled = scaler.fit_transform(allLapsDF)
 
 pca = PCA(n_components=2)
 Xpca = pca.fit_transform(scaled)
-hdb = hdbscan.HDBSCAN(min_cluster_size=20)
+hdb = HDBSCAN(min_cluster_size=20, max_cluster_size=50)
 hdb.fit(Xpca)
+labels = hdb.labels_
